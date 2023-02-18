7e6fc6b1ba7d7b47d1f60b69446c9196413603c5
Western <Western@KEVINS-DESKTOP>
2023-02-17 19:38:18 -0500

    Add project files.

---
+++ NoFeedbackOnExit/Main.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox.Game;
+using VRage.Plugins;
+
+namespace NoFeedbackOnExit
+{
+    public class Main : IPlugin
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(object gameInstance)
+        {
+            MyPlatformGameSettings.FEEDBACK_ON_EXIT = false;
+            MyPlatformGameSettings.FEEDBACK_URL = "";
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
